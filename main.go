fe35b0b48d36bfc05e8b4cab1739f5028f5b0edc
Tigran Simonyan <tamtface@ya.ru>
2025-12-19 17:22:40 +0300

    test

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{
-		"test": "test",
+		"test": "tesalanos",
 	})
 }
 
